d3e5cc2ea94ba131ba56b1dca60b76829fac9e81
Fatmagyul Chitakova <fchitakova@fchitakova2M7MM.vmware.com>
2023-08-22 18:40:25 +0300

    First commit

---
+++ src/main/java/com/fchitakova/giftmecloud/GiftMeCloudApplication.java
@@ -0,0 +1,13 @@
+package com.fchitakova.giftmecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftMeCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftMeCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fchitakova/giftmecloud/GiftMeCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fchitakova.giftmecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftMeCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
