7fa7e1b4fa00a8e2e28933ceb6bd9269a3bafc82
fchitakova <fchitakova@vmware.com>
2023-11-02 20:08:52 +0200

    Populate-Gifts-Feed-With-Gifts

--- src/main/java/com/fchitakova/giftmecloud/gift/controller/HomeController.java
+++ src/main/java/com/fchitakova/giftmecloud/gift/controller/HomeController.java
@@ -1,12 +1,16 @@
 package com.fchitakova.giftmecloud.gift.controller;
 
 import com.fchitakova.giftmecloud.gift.model.dto.GiftDTO;
+import com.fchitakova.giftmecloud.gift.repository.GiftRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+    private final GiftRepository giftRepository;
 
     @GetMapping("/")
     public String home(Model model) {
